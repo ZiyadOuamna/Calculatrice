e78f2fdbf43b98f1efd7e9360e527bfca2f558e1
Ziyad Ouamna <117644947+ZiyadOuamna@users.noreply.github.com>
2023-05-20 18:53:01 +0100

    Update script.js
    j'ai corriger la fonction qui supprime le dernier nombre

--- script.js
+++ script.js
@@ -3,21 +3,11 @@ let affichage = document.getElementById("barrewhite");
 function display(num){
     affichage.value += num;
 }
-function Delete2(){
-    affichage.value = affichage.value - 1/*slice(0, -1)*/;
-}
+
 function Delete1(){
-    affichage.value = ("");
+    affichage.value = "";
 }
-
-/* on peux faire ceci :
-let touche  = document.getElementsByClassName('.rrr').value;
-    touche[3].addEventListener("click",display(''));
-*/
-/* ou bien en peux utiliser une boucle : 
-for (let i=0 ;i<9;i++){
-    touche[i].addEventListener("click", function(display(i+1)))
+function Delete2(){
+    affichage.value = affichage.value.substring(0,affichage.value.length-1)/*slice(0, -1)*/;
+    //on a affichage.value sou forme d'une table et donc on est soustraire de affichage.value[0] jusqu'a affichage.value[le avant dernier nombre dans ce tableau est affichage.value.length - 1]
 }
-
-
-*/
